72fa17361a2cd1dc7c1dd8748df7977c12ca374e
itz-abdulfatai <134066346+itz-abdulfatai@users.noreply.github.com>
2024-04-18 17:56:55 +0100

    compile game into one page

--- js/bullet.js
+++ js/bullet.js
@@ -1,137 +1,137 @@
-class Bullet {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.width = 5;
-    this.height = 10;
-    this.element = document.createElement("div");
-    this.id = `bullet-${Math.floor(Math.random() * 999)}-${Math.floor(
-      Math.random() * 999
-    )}`;
-    this.zIndex = Math.floor(Math.random() * 80);
-  }
+// class Bullet {
+//   constructor(x, y) {
+//     this.x = x;
+//     this.y = y;
+//     this.width = 5;
+//     this.height = 10;
+//     this.element = document.createElement("div");
+//     this.id = `bullet-${Math.floor(Math.random() * 999)}-${Math.floor(
+//       Math.random() * 999
+//     )}`;
+//     this.zIndex = Math.floor(Math.random() * 80);
+//   }
 
-  draw() {
-    const bullet = this.element;
-    bullet.classList.add("bullet");
-    bullet.classList.add(`bullet-${this.id}`);
-    bullet.style.height = `${this.height}px`;
-    bullet.style.width = `${this.width}px`;
-    bullet.style.position = "absolute";
-    bullet.style.bottom = `${this.y}px`;
-    bullet.style.left = `${this.x}px`;
-    game.appendChild(bullet);
-    bullet.style.zIndex = this.zIndex;
-    bullet.dataset.bulletId = this.id;
-  }
-}
+//   draw() {
+//     const bullet = this.element;
+//     bullet.classList.add("bullet");
+//     bullet.classList.add(`bullet-${this.id}`);
+//     bullet.style.height = `${this.height}px`;
+//     bullet.style.width = `${this.width}px`;
+//     bullet.style.position = "absolute";
+//     bullet.style.bottom = `${this.y}px`;
+//     bullet.style.left = `${this.x}px`;
+//     game.appendChild(bullet);
+//     bullet.style.zIndex = this.zIndex;
+//     bullet.dataset.bulletId = this.id;
+//   }
+// }
 
-function shootBullet() {
-  const player = document.querySelector(".player");
-  const xPos = player.style.left;
-  const xPosNumber = xPos.split("px")[0];
-  const bullet = new Bullet(Number(xPosNumber) + 35, 20);
-  bullet.draw();
-  bullets.push(bullet);
-}
+// function shootBullet() {
+//   const player = document.querySelector(".player");
+//   const xPos = player.style.left;
+//   const xPosNumber = xPos.split("px")[0];
+//   const bullet = new Bullet(Number(xPosNumber) + 35, 20);
+//   bullet.draw();
+//   bullets.push(bullet);
+// }
 
-function moveBullet() {
-  bullets.forEach((bullet) => {
-    bullet.y += 10;
-    // the 10 here can be variable bullet speed if you would like to change it
-    bullet.draw();
-  });
-}
+// function moveBullet() {
+//   bullets.forEach((bullet) => {
+//     bullet.y += 10;
+//     // the 10 here can be variable bullet speed if you would like to change it
+//     bullet.draw();
+//   });
+// }
 
-setInterval(() => {
-  moveBullet();
-}, 1000 / 60);
-
-
-
-document.body.addEventListener("keydown", () => {
-  if (event.key == " " && !gameOver) {
-    shootBullet();
-  }
-});
-
-function checkBulletPosition() {
-  if (bullets) {
-    for (let i = 0; i < bullets.length; i++) {
-      const bullet = bullets[i];
-      const bulletElement = bullet.element;
-      const gameHeight = game.getBoundingClientRect().height;
-      if (bullet.y > gameHeight - 50) {
-        console.log("game height reached");
-        queueFree(bulletElement, bullets, i);
-      }
-    }
-  }
-}
-
-function checkForCollision() {
-  if (bullets) {
-    for (let i = 0; i < bullets.length; i++) {
-      const bullet = bullets[i];
-      const bulletElement = bullet.element;
-
-      const bulletPosition = bulletElement.getBoundingClientRect();
-      const bulletPositionX = bulletPosition.x;
-      const bulletPositionY = bulletPosition.y;
-
-      for (let i = 0; i < enemies.length; i++) {
-        const enemy = enemies[i];
-        const enemyElement = enemy.element;
-
-        const enemyPosition = enemyElement.getBoundingClientRect();
-        const enemyPositionX = enemyPosition.x;
-        const enemyPositionY = enemyPosition.y;
+// setInterval(() => {
+//   moveBullet();
+// }, 1000 / 60);
 
 
-        if (
-          bulletPosition.x < enemyPosition.x + enemyPosition.width &&
-          bulletPosition.x + bulletPosition.width > enemyPosition.x &&
-          bulletPosition.y < enemyPosition.y + enemyPosition.height &&
-          bulletPosition.y + bulletPosition.height > enemyPosition.y
-        ) {
-          console.log("enemy very close");
-          queueFree(bulletElement, bullets, i);
-          queueFree(enemyElement, enemies, i);
-          score += 5;
-          updateScore();
 
-        }
-      }
-    }
-  }
-}
+// document.body.addEventListener("keydown", () => {
+//   if (event.key == " " && !gameOver) {
+//     shootBullet();
+//   }
+// });
+
+// function checkBulletPosition() {
+//   if (bullets) {
+//     for (let i = 0; i < bullets.length; i++) {
+//       const bullet = bullets[i];
+//       const bulletElement = bullet.element;
+//       const gameHeight = game.getBoundingClientRect().height;
+//       if (bullet.y > gameHeight - 50) {
+//         console.log("game height reached");
+//         queueFree(bulletElement, bullets, i);
+//       }
+//     }
+//   }
+// }
+
+// function checkForCollision() {
+//   if (bullets) {
+//     for (let i = 0; i < bullets.length; i++) {
+//       const bullet = bullets[i];
+//       const bulletElement = bullet.element;
+
+//       const bulletPosition = bulletElement.getBoundingClientRect();
+//       const bulletPositionX = bulletPosition.x;
+//       const bulletPositionY = bulletPosition.y;
+
+//       for (let i = 0; i < enemies.length; i++) {
+//         const enemy = enemies[i];
+//         const enemyElement = enemy.element;
+
+//         const enemyPosition = enemyElement.getBoundingClientRect();
+//         const enemyPositionX = enemyPosition.x;
+//         const enemyPositionY = enemyPosition.y;
 
 
-setInterval(() => {
-  checkForCollision();
-}, 1000 / 20);
+//         if (
+//           bulletPosition.x < enemyPosition.x + enemyPosition.width &&
+//           bulletPosition.x + bulletPosition.width > enemyPosition.x &&
+//           bulletPosition.y < enemyPosition.y + enemyPosition.height &&
+//           bulletPosition.y + bulletPosition.height > enemyPosition.y
+//         ) {
+//           console.log("enemy very close");
+//           queueFree(bulletElement, bullets, i);
+//           queueFree(enemyElement, enemies, i);
+//           score += 5;
+//           updateScore();
 
-function updateScore() {
-  const scoreDisplay = document.querySelector(".score");
-  scoreDisplay.innerHTML = score;
-}
+//         }
+//       }
+//     }
+//   }
+// }
 
-// for residual bullets
 
-function clearBullets() {
-  const bulletsOnScreen = document.querySelectorAll(".bullet");
+// setInterval(() => {
+//   checkForCollision();
+// }, 1000 / 20);
 
-  bulletsOnScreen.forEach((bullet) => {
-    const bulletsOnScreenId = bullet.dataset.bulletId;
-    let bulletsInArray = [];
+// function updateScore() {
+//   const scoreDisplay = document.querySelector(".score");
+//   scoreDisplay.innerHTML = score;
+// }
 
-    bullets.forEach((bullet) => {
-      bulletsInArray.push(bullet.id);
-    });
+// // for residual bullets
 
-    if (!bulletsInArray.includes(bulletsOnScreenId)) {
-      bullet.remove();
-    }
-  });
+// function clearBullets() {
+//   const bulletsOnScreen = document.querySelectorAll(".bullet");
 
-}
+//   bulletsOnScreen.forEach((bullet) => {
+//     const bulletsOnScreenId = bullet.dataset.bulletId;
+//     let bulletsInArray = [];
+
+//     bullets.forEach((bullet) => {
+//       bulletsInArray.push(bullet.id);
+//     });
+
+//     if (!bulletsInArray.includes(bulletsOnScreenId)) {
+//       bullet.remove();
+//     }
+//   });
+
+// }
--- js/enemy.js
+++ js/enemy.js
@@ -1,70 +1,70 @@
 
-class Enemy {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.width = 70;
-    this.height = 50;
-    this.element = document.createElement("img");
-    this.src = `assets/enemy-sprite-${Math.floor(Math.random() * 7)}.png`;
-    this.id = `enemy-${Math.floor(Math.random() * 999)}-${Math.floor(
-      Math.random() * 999
-    )}`;
-    this.zIndex = 90;
-  }
+// class Enemy {
+//   constructor(x, y) {
+//     this.x = x;
+//     this.y = y;
+//     this.width = 70;
+//     this.height = 50;
+//     this.element = document.createElement("img");
+//     this.src = `assets/enemy-sprite-${Math.floor(Math.random() * 6)}.png`;
+//     this.id = `enemy-${Math.floor(Math.random() * 999)}-${Math.floor(
+//       Math.random() * 999
+//     )}`;
+//     this.zIndex = 90;
+//   }
 
-  draw() {
-    const enemy = this.element;
-    enemy.classList.add("enemy");
-    enemy.style.height = `${this.height}px`;
-    enemy.style.width = `${this.width}px`;
-    enemy.style.position = "absolute";
-    enemy.style.top = `${this.y}px`;
-    enemy.style.left = `${this.x}px`;
-    enemy.src = this.src;
-    enemy.style.objectFit = "cover";
-    game.appendChild(enemy);
-    enemy.style.zIndex = this.zIndex;
-    enemy.dataset.enemyId = this.id;
-  }
-}
+//   draw() {
+//     const enemy = this.element;
+//     enemy.classList.add("enemy");
+//     enemy.style.height = `${this.height}px`;
+//     enemy.style.width = `${this.width}px`;
+//     enemy.style.position = "absolute";
+//     enemy.style.top = `${this.y}px`;
+//     enemy.style.left = `${this.x}px`;
+//     enemy.src = this.src;
+//     enemy.style.objectFit = "cover";
+//     game.appendChild(enemy);
+//     enemy.style.zIndex = this.zIndex;
+//     enemy.dataset.enemyId = this.id;
+//   }
+// }
 
 
 
-function spawnEnemy() {
-  const enemy = new Enemy(Math.floor(Math.random() * (game.offsetWidth - 60)), 1);
-  enemy.draw();
-  enemies.push(enemy);
-}
+// function spawnEnemy() {
+//   const enemy = new Enemy(Math.floor(Math.random() * (game.offsetWidth - 60)), 1);
+//   enemy.draw();
+//   enemies.push(enemy);
+// }
 
-function moveEnemy() {
-  enemies.forEach((enemy) => {
-    //  tenemy speed can be changed in globals.js
-    enemy.y += enemySpeed;
-    enemy.draw();
-  });
-}
+// function moveEnemy() {
+//   enemies.forEach((enemy) => {
+//     //  enemy speed can be changed in globals.js
+//     enemy.y += enemySpeed;
+//     enemy.draw();
+//   });
+// }
 
-function checkEnemyPosition() {
-  if (enemies) {
-    for (let i = 0; i < enemies.length; i++) {
-      const enemy = enemies[i];
-      const enemyElement = enemy.element;
-      const gameHeight = game.getBoundingClientRect().height;
+// function checkEnemyPosition() {
+//   if (enemies) {
+//     for (let i = 0; i < enemies.length; i++) {
+//       const enemy = enemies[i];
+//       const enemyElement = enemy.element;
+//       const gameHeight = game.getBoundingClientRect().height;
 
-      if (enemy.y > gameHeight - 50) {
-        queueFree(enemyElement, enemies, i);
-        gameOver = true;
+//       if (enemy.y > gameHeight - 50) {
+//         queueFree(enemyElement, enemies, i);
+//         gameOver = true;
 
-      }
-    }
-  }
-}
+//       }
+//     }
+//   }
+// }
 
-function queueFree(element, parent, index) {
-    element.classList.add('gone')
-    element.remove();
+// function queueFree(element, parent, index) {
+//     element.classList.add('gone')
+//     element.remove();
 
-  parent.splice(index, 1);
+//   parent.splice(index, 1);
 
-}
+// }
